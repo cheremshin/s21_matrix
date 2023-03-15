217a2f74e6e1a6a589f74fe08c8647a678647e0a
Skyte Kelsi <skytekel@si-d9.nsk.21-school.ru>
2023-03-15 22:21:01 +0700

    feat: Complete logic

--- src/lib/s21_inverse_matrix.c
+++ src/lib/s21_inverse_matrix.c
@@ -8,7 +8,20 @@ int s21_inverse_matrix(matrix_t *A, matrix_t *result) {
     status = s21_determinant(A, &determinant);
 
     if (status == EXIT_SUCCESS && determinant != 0.0) {
-        
+        matrix_t *complements = (matrix_t *)malloc(sizeof(matrix_t));
+        s21_calc_complements(A, complements);
+
+        matrix_t *transpose = (matrix_t *)malloc(sizeof(matrix_t));
+        s21_transpose(complements, transpose);
+
+        s21_mult_number(transpose, 1/determinant, result);
+
+        s21_remove_matrix(complements);
+        s21_remove_matrix(transpose);
+        free(complements);
+        free(transpose);
+    } else {
+        status = EXIT_CALCULATION_ERROR;
     }
 
     return status;
