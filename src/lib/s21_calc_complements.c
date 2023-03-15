4c41a80ada7256b79bb9370f70d9f6f4685ddcd9
Skyte Kelsi <skytekel@si-d9.nsk.21-school.ru>
2023-03-15 20:08:51 +0700

    feat: Complete s21_calc_complements function

---
+++ src/lib/s21_calc_complements.c
@@ -0,0 +1,30 @@
+#include "s21_matrix.h"
+#include "s21_common.h"
+
+int s21_calc_complements(matrix_t *A, matrix_t *result) {
+    int status = EXIT_SUCCESS;
+
+    if ((A != NULL) && s21_check_on_square(A)) {
+        s21_create_matrix(A->rows, A->columns, result);
+
+        double determinant;
+        matrix_t *minor = (matrix_t *)malloc(sizeof(matrix_t));
+
+        for (int i = 0; i < A->rows; i++) {
+            for (int j = 0; j < A->columns; j++) {
+                s21_get_minor(A, i, j, minor);
+                s21_determinant(minor, &determinant);
+
+                result->matrix[i][j] = s21_sign(i, j) * determinant;
+
+                s21_remove_matrix(minor);
+            }
+        }
+
+        free(minor);
+    } else {
+        status = EXIT_CALCULATION_ERROR;
+    }
+
+    return status;
+}
