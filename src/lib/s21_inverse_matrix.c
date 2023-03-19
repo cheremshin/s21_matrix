24d1d843e639413de742cc958d346a6af4c4d827
cheremshin <sergey.cheremshin@gmail.com>
2023-03-19 22:48:33 +0700

    feat: Change temporary matrix to static allocation

--- src/lib/s21_inverse_matrix.c
+++ src/lib/s21_inverse_matrix.c
@@ -1,6 +1,8 @@
 #include "s21_matrix.h"
 #include "s21_common.h"
 
+#include <stdio.h>
+
 int s21_inverse_matrix(matrix_t *A, matrix_t *result) {
     int status = EXIT_SUCCESS;
 
@@ -8,18 +10,13 @@ int s21_inverse_matrix(matrix_t *A, matrix_t *result) {
     status = s21_determinant(A, &determinant);
 
     if (status == EXIT_SUCCESS && determinant != 0.0) {
-        matrix_t *complements = (matrix_t *)malloc(sizeof(matrix_t));
-        s21_calc_complements(A, complements);
+        matrix_t complements = {0, 0, 0};
+        matrix_t transpose = {0, 0, 0};
 
-        matrix_t *transpose = (matrix_t *)malloc(sizeof(matrix_t));
-        s21_transpose(complements, transpose);
+        s21_calc_complements(A, &complements);
+        s21_transpose(&complements, &transpose);
 
-        s21_mult_number(transpose, 1/determinant, result);
-
-        s21_remove_matrix(complements);
-        s21_remove_matrix(transpose);
-        free(complements);
-        free(transpose);
+        s21_mult_number(&transpose, 1/determinant, result);
     } else {
         status = EXIT_CALCULATION_ERROR;
     }
