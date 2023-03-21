cb89c7ab3b3dcda639aa80870fe320490fdb3cb8
cheremshin <sergey.cheremshin@gmail.com>
2023-03-21 13:45:43 +0700

    feat: Remove Gaussian method and add determinant for 1x1 matrix

--- src/lib/s21_determinant.c
+++ src/lib/s21_determinant.c
@@ -6,8 +6,8 @@ int s21_determinant(matrix_t *A, double *result) {
     int status = EXIT_SUCCESS;
 
     if ((A != NULL) && s21_check_on_square(A)) {
-        if (s21_diagonal_check(A)) {
-            *result = s21_gaussian_method(A);
+        if (A->rows == 1) {
+            *result = A->matrix[0][0];
         } else {
             *result = s21_minor_method(A);
         }
