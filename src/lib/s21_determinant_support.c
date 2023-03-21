1c3a16564c4f71ed83e5bdb0f7ea0280f9ef90df
cheremshin <sergey.cheremshin@gmail.com>
2023-03-21 13:46:40 +0700

    feat: Remove Gaussian method

--- src/lib/s21_determinant_support.c
+++ src/lib/s21_determinant_support.c
@@ -3,61 +3,6 @@
 
 #include <math.h>
 
-int s21_diagonal_check(matrix_t *A) {
-    int status = SUCCESS;
-
-    for (int i = 0; i < A->rows && status; i++) {
-        if (fabs(A->matrix[i][i]) < EPSILON) {
-            status = FAILURE;
-        }
-    }
-
-    return status;
-}
-
-double s21_gaussian_method(matrix_t *A) {
-    double determinant;
-
-    matrix_t *copy = (matrix_t *)malloc(sizeof(matrix_t));
-    s21_copy_matrix(A, copy);
-
-    double multiplier = 1;
-
-    for (int column = 0; column < copy->columns - 1; column++) {
-        multiplier *= s21_normalize(copy, column, column);
-        for (size_t row = column + 1; row < copy->rows; row++) {
-            s21_nullify(copy, row, column);
-        }
-    }
-
-    determinant = copy->matrix[copy->rows - 1][copy->columns - 1] * multiplier;
-
-    s21_remove_matrix(copy);
-    free(copy);
-
-    return determinant;
-}
-
-double s21_normalize(matrix_t *A, int row, int column) {
-    double divider = A->matrix[row][column];
-    for (int i = column; i < A->columns; i++) {
-        A->matrix[row][i] /= divider;
-    }
-
-    return divider;
-}
-
-void s21_nullify(matrix_t *A, int row, int column) {
-    double multiplier = A->matrix[row][column];
-    for (int i = column; i < A->columns; i++) {
-        A->matrix[row][i] -= A->matrix[column][i] * multiplier;
-    }
-}
-
-double s21_second_order_matrix_determinant(matrix_t *A) {
-    return (A->matrix[0][0] * A->matrix[1][1]) - (A->matrix[1][0] * A->matrix[0][1]);
-}
-
 double s21_minor_method(matrix_t *A) {
     double determinant = 0;
 
@@ -78,4 +23,8 @@ double s21_minor_method(matrix_t *A) {
     }
 
     return determinant;
+}
+
+double s21_second_order_matrix_determinant(matrix_t *A) {
+    return (A->matrix[0][0] * A->matrix[1][1]) - (A->matrix[1][0] * A->matrix[0][1]);
 }
