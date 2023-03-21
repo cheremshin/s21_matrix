a42d2ec531a15d06a3ec63cfe6a96111f8cb950e
cheremshin <sergey.cheremshin@gmail.com>
2023-03-21 13:47:13 +0700

    fix: Fixed SIGSEGV when 1x1 matrix incoming

--- src/lib/s21_inverse_matrix.c
+++ src/lib/s21_inverse_matrix.c
@@ -1,8 +1,6 @@
 #include "s21_matrix.h"
 #include "s21_common.h"
 
-#include <stdio.h>
-
 int s21_inverse_matrix(matrix_t *A, matrix_t *result) {
     int status = EXIT_SUCCESS;
 
@@ -10,13 +8,17 @@ int s21_inverse_matrix(matrix_t *A, matrix_t *result) {
     status = s21_determinant(A, &determinant);
 
     if (status == EXIT_SUCCESS && determinant != 0.0) {
-        matrix_t complements = {0, 0, 0};
-        matrix_t transpose = {0, 0, 0};
+        if (A->rows != 1) {
+            matrix_t complements = {0, 0, 0};
+            matrix_t transpose = {0, 0, 0};
 
-        s21_calc_complements(A, &complements);
-        s21_transpose(&complements, &transpose);
+            s21_calc_complements(A, &complements);
+            s21_transpose(&complements, &transpose);
 
-        s21_mult_number(&transpose, 1/determinant, result);
+            s21_mult_number(&transpose, 1/determinant, result);
+        } else {
+            s21_mult_number(A, 1/determinant, result);
+        }
     } else {
         status = EXIT_CALCULATION_ERROR;
     }
